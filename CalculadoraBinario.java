ba084b7d7419b8ed9393b91b29713c63c22e885e
j0hnv1 <john16almeida@gmail.com>
2026-08-14 22:19:36 -0300

    Add files via upload

---
+++ CalculadoraBinario.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class CalculadoraBinario {
+    public static void main(String[] args) {
+
+        Scanner scr = new Scanner(System.in);
+
+        System.out.println("coloque o valor em decimal");
+        int valordecimal = scr .nextInt();
+        
+        
+        int resolução = valordecimal / 2;
+
+        while (valordecimal > 0 ) { 
+            int resto = valordecimal % 2;
+            System.out.println(resto);
+            
+            valordecimal = valordecimal / 2;
+        }
+        
+        
+
+       
+
+        
+
+
+        
+
+        
+
+        
+    
+
+        
+        
+        
+        
+        
+        
+
+    }
+}
+    
+
